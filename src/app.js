8c0a2ade6cf48396c1f59ddb0634660ecd044ca5
YurizadaCMSP <cisampofc@gmail.com>
2025-12-26 15:52:10 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
-const queryRoute = require('./routes/query.route');
+const queryRoute = require('.src/routes/query.route');
 
 const app = express();
 
