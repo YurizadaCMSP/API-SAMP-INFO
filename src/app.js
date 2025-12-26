9eb4a375a9be4424b076a63bb7060fd2f0864440
YurizadaCMSP <cisampofc@gmail.com>
2025-12-26 00:20:10 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,49 +1,39 @@
+// app.js
 const express = require('express');
 const cors = require('cors');
 const queryRoute = require('./routes/query.route');
 
 const app = express();
 
-// Middlewares globais
-app.use(cors());
+// Middlewares
+app.use(cors({
+  origin: ['https://sampinfo.qzz.io', 'http://localhost:3000'],
+  methods: ['GET'],
+  credentials: true
+}));
+
 app.use(express.json());
 
-// Log de requisições
-app.use((req, res, next) => {
-  console.log(`${new Date().toISOString()} - ${req.method} ${req.path} - IP: ${req.ip}`);
-  next();
-});
+// Routes
+app.use('/query', queryRoute);
 
 // Health check
 app.get('/', (req, res) => {
   res.json({
+    service: 'SA-MP Info API',
     status: 'online',
-    message: 'SA-MP Info API',
     version: '1.0.0',
     endpoints: {
       query: '/query?ip=127.0.0.1&port=7777'
-    },
-    docs: 'https://github.com/seu-usuario/samp-info-api'
+    }
   });
 });
 
-// Rotas
-app.use('/query', queryRoute);
-
-// 404 Handler
+// 404
 app.use((req, res) => {
   res.status(404).json({
     error: 'Endpoint não encontrado',
-    message: 'Utilize /query?ip=<IP>&port=<PORTA>'
-  });
-});
-
-// Error Handler
-app.use((err, req, res, next) => {
-  console.error('Erro:', err);
-  res.status(500).json({
-    error: 'Erro interno do servidor',
-    message: err.message
+    availableEndpoints: ['/query']
   });
 });
 
