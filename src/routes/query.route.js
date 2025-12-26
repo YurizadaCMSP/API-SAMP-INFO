cb5a9577fbf08f3bfe27770abb49a691c6c31c49
YurizadaCMSP <cisampofc@gmail.com>
2025-12-26 00:20:33 -0300

    Update query.route.js

--- src/routes/query.route.js
+++ src/routes/query.route.js
@@ -1,9 +1,10 @@
+// routes/query.route.js
 const express = require('express');
 const router = express.Router();
 const queryController = require('../controllers/query.controller');
 const rateLimitMiddleware = require('../middlewares/rateLimit');
 
-// Aplica rate limit de 1 requisição a cada 5 minutos por IP
+// Aplica rate limit de 1 req a cada 5 minutos por IP
 router.get('/', rateLimitMiddleware, queryController.queryServer);
 
 module.exports = router;
